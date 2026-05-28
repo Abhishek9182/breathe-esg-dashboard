875a87d97e6b4ba33a01058ca615da06bd77d69a
Ramanji <t.g.l.ramanji@gamil.com>
2026-05-28 20:42:48 +0530

    fixed django api routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ Tooltip,
 Legend
 );
 
-const API_URL = "https://breathe-esg-dashboard-1-osno.onrender.com/api";
+const API_URL = "https://breathe-esg-dashboard-1-6icc.onrender.com/api/emissions/";
 function App(){
 
 const [dashboard,setDashboard]=useState({});
